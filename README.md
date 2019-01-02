fca2c7e37960ae87e2644aece5324636c1995e49
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-02 17:43:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# particle-generator-p5
---
+++ sketch.js
@@ -0,0 +1,50 @@
+let particles = [];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
+}
+
+function draw() {
+	background(0);
+	let p = new Particle();
+	particles.push(p);
+
+	for (let i = particles.length-1; i >= 0; i--) {
+		
+		particles[i].update();
+		particles[i].show();
+		
+		if (particles[i].finished()) {
+			particles.splice(i, 1);
+		}
+	}
+}
+
+class Particle {
+	constructor() {
+		this.x = 300;
+		this.y = 380;
+		this.vx = random(-1, 1);
+		this.vy = random(-5, -1);
+		this.alpha = 255;
+
+	}
+
+	finished() {
+		return this.alpha < 0;
+	}
+
+
+	update() {
+		this.x += this.vx;
+		this.y += this.vy;
+		this.alpha -= 5;
+	}
+
+	show() {
+		noStroke();
+		fill(200, this.alpha);
+		ellipse(this.x, this.y, 16);
+	}
+}
